4db41dbe365d3a1d79eb5234eab2725a173c3423
sos11gg1 <sos11gg1@yahoo.com.tw>
2020-01-03 14:32:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hw20/ProgressDialogUtil.java
@@ -0,0 +1,61 @@
+package com.example.hw20;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class ProgressDialogUtil {
+    private static AlertDialog mAlertDialog;
+
+    /**
+     * 彈出耗時對話方塊
+     * @param context
+     */
+    public static void showProgressDialog(Context context) {
+        if (mAlertDialog == null) {
+            mAlertDialog = new AlertDialog.Builder(context, R.style.CustomProgressDialog).create();
+        }
+
+        View loadView = LayoutInflater.from(context).inflate(R.layout.custom_progress_dialog_view, null);
+        mAlertDialog.setView(loadView, 0, 0, 0, 0);
+        mAlertDialog.setCanceledOnTouchOutside(false);
+
+        TextView tvTip = loadView.findViewById(R.id.tvTip);
+        tvTip.setText("載入中...");
+
+        mAlertDialog.show();
+    }
+
+    public static void showProgressDialog(Context context, String tip) {
+        if (TextUtils.isEmpty(tip)) {
+            tip = "載入中...";
+        }
+
+        if (mAlertDialog == null) {
+            mAlertDialog = new AlertDialog.Builder(context, R.style.CustomProgressDialog).create();
+        }
+
+        View loadView = LayoutInflater.from(context).inflate(R.layout.custom_progress_dialog_view, null);
+        mAlertDialog.setView(loadView, 0, 0, 0, 0);
+        mAlertDialog.setCanceledOnTouchOutside(false);
+
+        TextView tvTip = loadView.findViewById(R.id.tvTip);
+        tvTip.setText(tip);
+
+        mAlertDialog.show();
+    }
+
+    /**
+     * 隱藏耗時對話方塊
+     */
+    public static void dismiss() {
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            mAlertDialog.dismiss();
+        }
+    }
+}
+
